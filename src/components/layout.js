6784ebf014b1ef6f0e3882f30fefa8927734f690
Tesfa Yohannes <tmyohannes@gmail.com>
2021-09-10 17:03:22 +0800

    Add metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'Wellness at Innovation Tower Calendar' },
+              { name: 'keywords', content: 'wellness, meditation' },
             ]}
           >
             <html lang="en" />
